376226645ed9cf468c7fccdced87628273d22314
Siny <sinydaniel93@gmail.com>
2022-10-12 18:14:09 -0400

    subnet creation

--- subnets.tf
+++ subnets.tf
@@ -5,7 +5,7 @@ resource "aws_subnet" "public-subnets" {
   cidr_block = var.PUBLIC_SUBNET_CIDR[count.index]
 
   tags = {
-    Name = "public-subnets"
+    Name = "public-subnets-${count.index}"
   }
 }
 
@@ -15,6 +15,6 @@ resource "aws_subnet" "private-subnets" {
   cidr_block = var.PRIVATE_SUBNET_CIDR[count.index]
 
   tags = {
-    Name = "private-subnets"
+    Name = "private-subnets-${count.index}"
   }
 }
