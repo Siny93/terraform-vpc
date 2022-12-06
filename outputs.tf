26c2f9eb49184e47eb0fba1cfb7deda26d47121d
Siny <sinydaniel93@gmail.com>
2022-12-05 20:14:16 -0500

    outputs

--- outputs.tf
+++ outputs.tf
@@ -22,11 +22,11 @@ output "PRIVATE_SUBNET_CIDR" {
   value = var.PRIVATE_SUBNET_CIDR
 }
 
-//output "DEFAULT_VPC_ID" {
-//  value = data.aws_vpc.default.id
-//}
-//
-//output "DEFAULT_VPC_CIDR" {
-//  value = data.aws_vpc.default.cidr_block
-//}
+output "DEFAULT_VPC_ID" {
+  value = data.aws_vpc.default.id
+}
+
+output "DEFAULT_VPC_CIDR" {
+  value = data.aws_vpc.default.cidr_block
+}
 
